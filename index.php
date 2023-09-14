d74851e6fb291c55876a3a29f1e8f07d6af0cd32
TEKINT098 <tekint098@BTEKINT098U.biltek.local>
2023-09-14 16:49:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+function asalSayiMi($sayi){
+    if($sayi <= 1){
+        return false;
+    }
+    if($sayi == 2){
+        return true;
+    }
+
+    for ($i = 2; $i <= $sayi / 2; $i++){
+        if($sayi % $i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+$sayi = 17;
+if(asalSayiMi($sayi)){
+    echo $sayi . "sayısı asaldır";
+
+}else{
+    echo $sayi . "sayısı asal değildir";
+}
+
